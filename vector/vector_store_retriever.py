a24c873e83baca627af6294afe060e4d0a6df8b7
HyperdriveHustle <503147114@qq.com>
2023-05-11 14:57:32 +0800

    remove memory stream from retriever

--- vector/vector_store_retriever.py
+++ vector/vector_store_retriever.py
@@ -1,7 +1,12 @@
 """
 Retriever that combines embedding similarity with recency in retrieving values.
-TODO 修改 memory stream 逻辑，聊天记录和 memory 拆分
+修改方案：
+memory_stream 和 vectorstore 解耦
+- memory_stream 存聊天上文，在 retriever 里删除
+- vectorstore 存聊天上下文 and 一些 reflection，实时写 & 实时读
+
 """
+import os
 from copy import deepcopy
 from datetime import datetime
 from typing import Any, Dict, List, Optional, Tuple
@@ -28,8 +33,8 @@ class TimeWeightedVectorStoreRetriever(BaseRetriever, BaseModel):
     """Keyword arguments to pass to the vectorstore similarity search."""
 
     # TODO: abstract as a queue
-    # 作为消息快照的存储，跟原本的 memory 解耦
-    memory_stream: List[Document] = Field(default_factory=list)
+    # 2023/5/11 删除 memory_stream 相关内容，消息记录用其他方式进行存储
+    # memory_stream: List[Document] = Field(default_factory=list)
     """The memory_stream of documents to search through."""
 
     decay_rate: float = Field(default=0.01)
@@ -58,7 +63,10 @@ class TimeWeightedVectorStoreRetriever(BaseRetriever, BaseModel):
         vector_relevance: Optional[float],
         current_time: datetime,
     ) -> float:
-        """Return the combined score for a document."""
+        """
+        Return the combined score for a document.
+        根据时间间隔计算 score 衰减情况
+        """
         hours_passed = _get_hours_passed(
             current_time,
             document.metadata["last_accessed_at"],
@@ -72,33 +80,42 @@ class TimeWeightedVectorStoreRetriever(BaseRetriever, BaseModel):
         return score
 
     def get_salient_docs(self, query: str) -> Dict[int, Tuple[Document, float]]:
-        """Return documents that are salient to the query."""
+        """
+        Return documents that are salient to the query.
+        从向量库中检索与 query 最相似的内容
+        """
         docs_and_scores: List[Tuple[Document, float]]
         docs_and_scores = self.vectorstore.similarity_search_with_relevance_scores(
             query, **self.search_kwargs
         )
         results = {}
-        # TODO 原代码里的 buffer id 怀疑是想记录现在内存 or redis 里是否存了聊天，从而从 memory  stream 里拿
-        # TODO 即 memory-stream 的内容 buffer-id 是一一对应的，但是目前还没看出来有更多的作用
         for fetched_doc, relevance in docs_and_scores:
             if "buffer_idx" in fetched_doc.metadata:
                 buffer_idx = fetched_doc.metadata["buffer_idx"]
                 # doc = self.memory_stream[buffer_idx]
-                doc = fetched_doc
-                results[buffer_idx] = (doc, relevance)
+                results[buffer_idx] = (fetched_doc, relevance)
         return results
 
     def get_relevant_documents(self, query: str) -> List[Document]:
         """
         Return documents that are relevant to the query.
+        根据向量相似度找到最相似 k 个doc 返回
         """
         current_time = datetime.now()
-        docs_and_scores = {
-            doc.metadata["buffer_idx"]: (doc, self.default_salience)
-            for doc in self.memory_stream[-self.k :]
-        }
+        # 1. 先从 memory stream 中找最近的 k 个消息记录，没有相关性得分
+        # docs_and_scores = {
+        #     doc.metadata["buffer_idx"]: (doc, self.default_salience)
+        #     for doc in self.memory_stream[-self.k:]
+        # }
         # If a doc is considered salient, update the salience score
-        docs_and_scores.update(self.get_salient_docs(query))
+        # 2. 更新与 query 最相似的doc
+        # 这里不仅更新从向量数据库中检索到的最相似的 doc 的 score
+        # 而且 如果之前不在 docs_and_scores 里的部分也会被增加进去
+        # docs_and_scores.update(self.get_salient_docs(query))
+
+        # 2023.5.11 更新: 只找到最相似的 document 不考虑上文的消息记录
+        docs_and_scores = self.get_salient_docs(query)
+        # 根据时间间隔计算相关性得分 score 的衰减情况（如果该doc 还包含 importance 得分之类的，则要加进去
         rescored_docs = [
             (doc, self._get_combined_score(doc, relevance, current_time))
             for doc, relevance in docs_and_scores.values()
@@ -109,9 +126,15 @@ class TimeWeightedVectorStoreRetriever(BaseRetriever, BaseModel):
         current_time = datetime.now()
         for doc, _ in rescored_docs[: self.k]:
             # TODO: Update vector store doc once `update` method is exposed.
-            buffered_doc = self.memory_stream[doc.metadata["buffer_idx"]]
-            buffered_doc.metadata["last_accessed_at"] = current_time
-            result.append(buffered_doc)
+            # 原代码逻辑：buffered_doc = self.memory_stream[doc.metadata["buffer_idx"]]
+            # 修改后：直接取
+            doc.metadata["last_accessed_at"] = current_time
+            result.append(doc)
+        print("---" * 20)
+        print("\t[get_relevant_documents] result")
+        for r in result:
+            print(f"\t\ttes = {r}")
+        print("---" * 20)
         return result
 
     def load_memories_from_local(
@@ -120,20 +143,12 @@ class TimeWeightedVectorStoreRetriever(BaseRetriever, BaseModel):
         """
         从本地 fassi 加载 memory
         TODO 定制方法，仅针对 faiss （用于加载记忆调试 memory 逻辑，后面替换成 milvus 后可删除）
+        2023/5/11 删除加载时存 memory stream 的逻辑
         :return:
         """
         self.vectorstore = self.vectorstore.load_local(
             folder_path, embeddings, index_name
         )
-        # 按照现在的逻辑需要把所有的内容都加入 memory stream 中
-        memory_stream = {}
-        for index, doc_id in self.vectorstore.index_to_docstore_id.items():
-            doc = self.vectorstore.docstore.search(doc_id)
-            memory_stream[doc.metadata['buffer_idx']] = doc
-        # memory stream 中存的是list 顺序是按照 buffer id 来的
-        # 要考虑读取时候顺序被打乱的情况（做排序）
-        memory_stream = sorted(memory_stream.items(), key=lambda x:x[0])
-        self.memory_stream = [x[1] for x in memory_stream]
 
     def save_memories_to_local(self, folder_path: str):
         '''
@@ -148,17 +163,22 @@ class TimeWeightedVectorStoreRetriever(BaseRetriever, BaseModel):
         raise NotImplementedError
 
     def add_documents(self, documents: List[Document], **kwargs: Any) -> List[str]:
-        """Add documents to vectorstore."""
+        """
+        Add documents to vectorstore.
+        """
         current_time = kwargs.get("current_time", datetime.now())
         # Avoid mutating input documents
         dup_docs = [deepcopy(d) for d in documents]
+        # 更新记忆的访问时间
         for i, doc in enumerate(dup_docs):
             if "last_accessed_at" not in doc.metadata:
                 doc.metadata["last_accessed_at"] = current_time
             if "created_at" not in doc.metadata:
                 doc.metadata["created_at"] = current_time
-            doc.metadata["buffer_idx"] = len(self.memory_stream) + i
-        self.memory_stream.extend(dup_docs)
+            # 删除 buffer_idx 将来和 context 统一设置后，从上一层传进来
+            # doc.metadata["buffer_idx"] = len(self.memory_stream) + i
+        # 2023/5/11 解耦 memory stream
+        # self.memory_stream.extend(dup_docs)
         return self.vectorstore.add_documents(dup_docs, **kwargs)
 
     async def aadd_documents(
@@ -173,6 +193,6 @@ class TimeWeightedVectorStoreRetriever(BaseRetriever, BaseModel):
                 doc.metadata["last_accessed_at"] = current_time
             if "created_at" not in doc.metadata:
                 doc.metadata["created_at"] = current_time
-            doc.metadata["buffer_idx"] = len(self.memory_stream) + i
-        self.memory_stream.extend(dup_docs)
+        #     doc.metadata["buffer_idx"] = len(self.memory_stream) + i
+        # self.memory_stream.extend(dup_docs)
         return await self.vectorstore.aadd_documents(dup_docs, **kwargs)
